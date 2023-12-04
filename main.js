614803759c9f76065c71339c2313789dd2942a12
Lucas Schünemann <88516015+lucasschunemann@users.noreply.github.com>
2023-12-04 14:12:38 -0300

    Arquivos adicionados por upload

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-    $("#formTarefa").submit(function(event) {
-      event.preventDefault(); 
-
-      var novaTarefa = $("#tarefa").val();
-
-      $("ul").append("<li>" + novaTarefa + "</li>");
-
-      $("#tarefa").val("");
-    });
-
-    $("ul").on("click", "li", function() {
-      $(this).toggleClass("completed");
+    // Aplicar máscaras aos campos usando o jQuery Mask Plugin
+    $('#telefone').mask('(00) 0000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+  
+    // Adicione mais lógica JavaScript conforme necessário, como validações, envio do formulário, etc.
+    $('#cadastroForm').submit(function(event) {
+      event.preventDefault();
+      // Lógica de envio do formulário ou validação aqui
+      console.log('Formulário enviado!');
     });
   });
+  
